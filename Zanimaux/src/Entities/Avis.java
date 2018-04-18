0ac30dfaba474e94a53fa50794d5328f44c1ce6a
MarouaArifa <36274254+MarouaArifa@users.noreply.github.com>
2018-04-18 12:02:47 +0100

    New project

---
+++ Zanimaux/src/Entities/Avis.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Azza
+ */
+public class Avis {
+    private String id;
+    private String idParc;
+    private double avis;
+    private String cinUser;
+
+    public Avis() {
+    }
+
+    public Avis(String id, String idParc, double avis, String cinUser) {
+        this.id = id;
+        this.idParc = idParc;
+        this.avis = avis;
+        this.cinUser = cinUser;
+    }
+
+    public Avis(String idParc, double avis, String cinUser) {
+        this.idParc = idParc;
+        this.avis = avis;
+        this.cinUser = cinUser;
+    }
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        hash = 89 * hash + Objects.hashCode(this.idParc);
+        hash = 89 * hash + (int) (Double.doubleToLongBits(this.avis) ^ (Double.doubleToLongBits(this.avis) >>> 32));
+        hash = 89 * hash + Objects.hashCode(this.cinUser);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Avis other = (Avis) obj;
+        if (Double.doubleToLongBits(this.avis) != Double.doubleToLongBits(other.avis)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.idParc, other.idParc)) {
+            return false;
+        }
+        if (!Objects.equals(this.cinUser, other.cinUser)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdParc() {
+        return idParc;
+    }
+
+    public void setIdParc(String idParc) {
+        this.idParc = idParc;
+    }
+
+    public double getAvis() {
+        return avis;
+    }
+
+    public void setAvis(double avis) {
+        this.avis = avis;
+    }
+
+    public String getCinUser() {
+        return cinUser;
+    }
+
+    public void setCinUser(String cinUser) {
+        this.cinUser = cinUser;
+    }
+
+    @Override
+    public String toString() {
+        return "Avis{" + "id=" + id + ", idParc=" + idParc + ", avis=" + avis + ", cinUser=" + cinUser + '}';
+    }
+
+    
+    
+}
---
+++ Zanimaux/src/Entities/Questionnaire_chien.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+/**
+ *
+ * @author Azza
+ */
+public class Questionnaire_chien {
+     private int id;
+    private int race;
+    private String tolererChien ;
+    private String calme;
+    private String affectueux;
+    private String chutePoils;
+    private String intelligent;
+    private String tolererChat;
+
+    public Questionnaire_chien() {
+    }
+
+    public Questionnaire_chien(int id, int race, String tolererChien, String calme, String affectueux, String chutePoils, String intelligent, String tolererChat) {
+        this.id = id;
+        this.race = race;
+        this.tolererChien = tolererChien;
+        this.calme = calme;
+        this.affectueux = affectueux;
+        this.chutePoils = chutePoils;
+        this.intelligent = intelligent;
+        this.tolererChat = tolererChat;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRace() {
+        return race;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 11 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Questionnaire_chien other = (Questionnaire_chien) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    public void setRace(int race) {
+        this.race = race;
+    }
+
+    public String getTolererChien() {
+        return tolererChien;
+    }
+
+    public void setTolererChien(String tolererChien) {
+        this.tolererChien = tolererChien;
+    }
+
+    public String getCalme() {
+        return calme;
+    }
+
+    public void setCalme(String calme) {
+        this.calme = calme;
+    }
+
+    public String getAffectueux() {
+        return affectueux;
+    }
+
+    public void setAffectueux(String affectueux) {
+        this.affectueux = affectueux;
+    }
+
+    public String getChutePoils() {
+        return chutePoils;
+    }
+
+    public void setChutePoils(String chutePoils) {
+        this.chutePoils = chutePoils;
+    }
+
+    public String getIntelligent() {
+        return intelligent;
+    }
+
+    public void setIntelligent(String intelligent) {
+        this.intelligent = intelligent;
+    }
+
+    public String getTolererChat() {
+        return tolererChat;
+    }
+
+    public void setTolererChat(String tolererChat) {
+        this.tolererChat = tolererChat;
+    }
+
+    @Override
+    public String toString() {
+        return "Questionnaire_chien{" + "id=" + id + ", race=" + race + ", tolererChien=" + tolererChien + ", calme=" + calme + ", affectueux=" + affectueux + ", chutePoils=" + chutePoils + ", intelligent=" + intelligent + ", tolererChat=" + tolererChat + '}';
+    }
+
+    
+    
+}

73b4f66619796a90e55fd7e08840d2b108df4792
MariamRiahi <mariam.riahi@esprit.tn>
2018-04-28 22:59:47 +0100

    webservices

---
+++ Zanimaux/src/com/mycompany/webService/Selectnotif2.php
@@ -0,0 +1,24 @@
+<?php
+   //open connection to mysql db
+    $connection = mysqli_connect("localhost","root","","nouvelle") or die("Error " . mysqli_error($connection));
+mysqli_set_charset($connection, "utf8");
+    //fetch table rows from mysql db
+$vu= $_GET['vu'];
+$id_notif= $_GET['id_notif'];
+
+    $sql = "SELECT cin FROM notif_user_rdv WHERE (vu = '$vu') AND  (id_notif = '$id_notif')";
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+
+    //create an array
+ //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+		
+    }
+  echo json_encode($emparray);
+
+    //close the db connection
+    mysqli_close($connection);
+?>
